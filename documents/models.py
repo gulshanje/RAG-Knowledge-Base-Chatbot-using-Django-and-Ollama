edde6ba130f1dfa5409ee703ad6cc011d709a238
Gulshan <gulshan.kumar.dsa@gmail.com>
2026-06-05 02:16:06 +0300

    Added Document model

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Document(models.Model):
+    title = models.CharField(max_length=255)
 
-# Create your models here.
+    uploaded_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    file = models.FileField(
+        upload_to="documents/"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
