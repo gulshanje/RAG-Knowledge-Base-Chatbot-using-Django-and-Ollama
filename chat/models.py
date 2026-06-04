b597651cc63bd0ef5417bcce833c02f1aced84e9
Gulshan <gulshan.kumar.dsa@gmail.com>
2026-06-05 02:17:01 +0300

    Added ChatSession model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+class ChatSession(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
 
-# Create your models here.
+    title = models.CharField(
+        max_length=255
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
