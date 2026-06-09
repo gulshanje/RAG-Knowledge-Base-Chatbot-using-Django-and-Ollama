418c15d32e420d736b0cd2fad56f2fcf6848a753
Gulshan <gulshan.kumar.dsa@gmail.com>
2026-06-09 23:43:17 +0300

    Added documents view

--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .models import Document
+from .serializers import DocumentSerializer
+
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+def test_view(request):
+    return JsonResponse({"message": "working"})
+
+class DocumentUploadView(
+    generics.CreateAPIView
+):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+    permission_classes = [
+        AllowAny
+    ]
+   
+    def perform_create(self, serializer):
+        user = User.objects.first()
+        print(type(user))
+        serializer.save(uploaded_by=user)
+    
+    
+    
