d8942340b544f56bfaf5a856cf0f3de751f8b62c
Gulshan <gulshan.kumar.dsa@gmail.com>
2026-06-09 23:42:35 +0300

    Added chatsession mode

--- chat/models.py
+++ chat/models.py
@@ -13,3 +13,25 @@ class ChatSession(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True
     )
+    
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+
+    session = models.ForeignKey(
+        ChatSession,
+        on_delete=models.CASCADE,
+        related_name="messages"
+    )
+
+    role = models.CharField(
+        max_length=20
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
